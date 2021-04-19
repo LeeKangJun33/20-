64a9ff7ef090bccaa5dbd4cca2a1cc7163fedcc0
LeeKangJun33 <62988674+LeeKangJun33@users.noreply.github.com>
2021-04-19 12:29:22 +0900

    Add files via upload

---
+++ WebTest/src/main/java/com/Web/samplepage/about/AboutRepository.java
@@ -0,0 +1,7 @@
+package com.Web.samplepage.about;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AboutRepository extends JpaRepository<About, Long> {
+    About findTopByOrderByIdDesc();
+}
